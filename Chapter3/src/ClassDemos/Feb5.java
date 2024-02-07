632f24436ca3fd39b00921cccfe19000077cdd42
amabdalla <amabdalla@E815WL230006.org.cbe.ab.ca>
2024-02-07 09:10:10 -0700

    9:10AM

---
+++ Chapter3/src/ClassDemos/Feb5.java
@@ -0,0 +1,33 @@
+package ClassDemos;
+
+public class Feb5 
+{
+
+	public static void main(String[] args) 
+	{
+		int x1;//Declaration
+		x1 = 7;//assignment
+		System.out.println(x1);
+		
+		int y = 8;
+		x1 = x1 + y;
+		System.out.println(x1);
+		
+		x1 = 99;
+		
+		
+		double grade = 77.8;
+		
+		boolean lights = true;
+		System.out.println(lights);
+		
+		
+		int x;
+		x = 5;
+		System.out.println(x);
+		
+		
+
+	}
+
+}
---
+++ Chapter3/src/ClassDemos/Feb7.java
@@ -0,0 +1,10 @@
+package ClassDemos;
+
+public class Feb7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
