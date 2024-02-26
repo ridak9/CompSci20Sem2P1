eb3da896d26eeb6cfc4a0b3c528ad02c859159ec
amabdalla <amabdalla@E815WL230006.org.cbe.ab.ca>
2024-02-26 09:12:53 -0700

    9:13AM

---
+++ Chapter5/src/ClassDemos/Feb26.java
@@ -0,0 +1,26 @@
+package ClassDemos;
+
+public class Feb26 
+{
+
+	public static void main(String[] args) 
+	{
+		int randNum;
+		int sequenceLength = 0;
+		
+		do
+		{	//Generate random numbers between 1 - 11
+			randNum = (int)(11 * Math.random());
+			
+			//Output each random number
+			System.out.println(randNum + " ");
+			
+			sequenceLength += 1;//increment the sequenceLength by 1			
+			
+		}while(randNum != 0);
+		
+		System.out.println("\n Length of sequence was "+sequenceLength);
+
+	}
+
+}
