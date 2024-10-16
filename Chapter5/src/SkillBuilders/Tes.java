66e0582135d2386e6fe833e59d189d5f9faf5b5d
amabdalla <amabdalla@E815WL230006.org.cbe.ab.ca>
2024-10-16 08:27:54 -0600

    8:27AM

---
+++ Chapter5/src/SkillBuilders/Tes.java
@@ -0,0 +1,13 @@
+package SkillBuilders;
+
+public class Tes 
+{
+
+	public static void main(String[] args) 
+	{
+		int x = 5; 
+		do {  x = x + 2; System.out.println(x);} while (x < 120);
+//
+	}
+
+}
